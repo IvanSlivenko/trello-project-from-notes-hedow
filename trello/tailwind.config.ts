3e9dc7f5205f16758316d83b2fdad4d5dea37b48
IvanSlivenko <ivan8822@ukr.net>
2025-06-02 18:58:32 +0300

    02-06-2025-18-58

---
+++ trello/tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{html,js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{html,js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{html,js,ts,jsx,tsx,mdx}',
+    './node_modules/flowbite/**/*.js',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
+};
+
+export default config;
